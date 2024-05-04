f7d7d346f1419d162b481253bcc74086380e377d
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-04 12:21:03 +0200

    Create project barebone.

---
+++ Accedo/Accedo/AccedoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccedoApp.swift
+//  Accedo
+//
+//  Created by Sajad Vishkai on 2024-05-04.
+//
+
+import SwiftUI
+
+@main
+struct AccedoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
