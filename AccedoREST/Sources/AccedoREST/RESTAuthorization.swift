d786d06a0e6e91c72b4e1ddc6eb20436a72a0bce
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 14:12:31 +0200

    Update RESTAuthorization.swift

--- AccedoREST/Sources/AccedoREST/RESTAuthorization.swift
+++ AccedoREST/Sources/AccedoREST/RESTAuthorization.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-protocol RESTAuthorizationProvider {
+public protocol RESTAuthorizationProvider {
     func getAuthorization() throws -> REST.Authorization
 }
 
@@ -8,14 +8,14 @@ extension REST {
     public typealias Authorization = String
     public typealias AuthorizationProvider = () -> Authorization
 
-    struct AccessTokenProvider: RESTAuthorizationProvider {
+    public struct AccessTokenProvider: RESTAuthorizationProvider {
         private let accessTokenResolver: (() throws -> Authorization)
 
-        init(accessToken: @escaping (() throws -> Authorization)) {
+        public init(accessToken: @escaping (() throws -> Authorization)) {
             self.accessTokenResolver = accessToken
         }
 
-        func getAuthorization() throws -> REST.Authorization {
+        public func getAuthorization() throws -> REST.Authorization {
             try accessTokenResolver()
         }
     }
