b5d9e80f1bf3360dea4e0b4b63c8ae83de0b6cc6
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 13:51:32 +0200

    Create RESTResponseHandler.swift

---
+++ AccedoREST/Sources/AccedoREST/RESTResponseHandler.swift
@@ -0,0 +1,65 @@
+import Foundation
+
+protocol RESTResponseHandler<Success> {
+    associatedtype Success
+
+    func handleURLResponse(
+        _ response: HTTPURLResponse,
+        data: Data
+    ) -> REST.ResponseHandlerResult<Success>
+}
+
+extension REST {
+    /// Responser handler result type.
+    enum ResponseHandlerResult<Success> {
+        /// Response handler succeeded and produced a value.
+        case success(Success)
+
+        /// Response handler succeeded and returned a block that decodes the value.
+        case decoding(_ decoderBlock: () throws -> Success)
+
+        /// Response handler received the response that it cannot handle.
+        /// Server error response is attached when available.
+        case unhandledResponse(ServerErrorResponse?)
+    }
+
+    final class AnyResponseHandler<Success>: RESTResponseHandler {
+        public typealias HandlerBlock = (HTTPURLResponse, Data) -> REST.ResponseHandlerResult<Success>
+
+        private let handlerBlock: HandlerBlock
+
+        public init(_ block: @escaping HandlerBlock) {
+            handlerBlock = block
+        }
+
+        public func handleURLResponse(
+            _ response: HTTPURLResponse,
+            data: Data
+        ) -> REST.ResponseHandlerResult<Success> {
+            handlerBlock(response, data)
+        }
+    }
+
+    /// Returns default response handler that parses JSON response into the
+    /// given `Decodable` type when it encounters HTTP `2xx` code, otherwise
+    /// attempts to decode the server error.
+    static func defaultResponseHandler<T: Decodable>(
+        decoding type: T.Type,
+        with decoder: JSONDecoder
+    ) -> AnyResponseHandler<T> {
+        AnyResponseHandler { response, data in
+            if HTTPStatus.isSuccess(response.statusCode) {
+                return .decoding {
+                    try decoder.decode(type, from: data)
+                }
+            } else {
+                return .unhandledResponse(
+                    try? decoder.decode(
+                        ServerErrorResponse.self,
+                        from: data
+                    )
+                )
+            }
+        }
+    }
+}
