4448658d85aa8e66d43c83a842683203db687bad
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 17:14:15 +0200

    Update AccedoDBTests.swift

--- AccedoDB/Tests/AccedoDBTests/AccedoDBTests.swift
+++ AccedoDB/Tests/AccedoDBTests/AccedoDBTests.swift
@@ -1,12 +1,94 @@
 import XCTest
 @testable import AccedoDB
 
-final class AccedoDBTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+private let cacheName = "Test"
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+final class AccedoDBSmokeTests: XCTestCase {
+    @DatabaseActor
+    func testWriteToCache() throws {
+        let cache = Cache<Int, String>(name: cacheName)
+
+        let mockKey = 1
+        let mockValue = "Salam"
+
+        cache.insert(mockValue, forKey: mockKey)
+
+        XCTAssertNotNil(try? cache.value(forKey: mockKey))
+
+        cache.removeValue(forKey: mockKey)
+    }
+
+    @DatabaseActor
+    func testWriteCacheToFile() throws {
+        let cache = Cache<Int, String>(name: cacheName)
+
+        let mockKey = 1
+        let mockValue = "Salam"
+
+        cache.insert(mockValue, forKey: mockKey)
+
+        XCTAssertNotNil(try? cache.value(forKey: mockKey))
+
+        XCTAssertNoThrow(try cache.saveToDisk())
+    }
+
+    @DatabaseActor
+    func testWriteCacheToFile_RawRetrieve() throws {
+        let cache = Cache<Int, String>(name: cacheName)
+
+        let mockKey = 1
+        let mockValue = "Salam"
+
+        cache.insert(mockValue, forKey: mockKey)
+
+        XCTAssertNotNil(try? cache.value(forKey: mockKey))
+
+        XCTAssertNoThrow(try cache.saveToDisk())
+
+        let fileManager = FileManager.default
+
+        let folderURLs = fileManager.urls(
+            for: .cachesDirectory,
+            in: .userDomainMask
+        )
+
+        let fileURL = folderURLs[0].appendingPathComponent(cacheName + ".cache")
+
+        let data = try Data(contentsOf: fileURL, options: .uncached)
+        let rawCache = String(data: data, encoding: .utf8)
+
+        XCTAssertNotNil(rawCache)
+        XCTAssertEqual(rawCache?.isEmpty, false)
+    }
+
+    @DatabaseActor
+    func testWriteCacheToFile_CodableRetrieve() throws {
+        let cache = Cache<Int, String>(name: cacheName)
+
+        let mockKey = 1
+        let mockValue = "Salam"
+
+        cache.insert(mockValue, forKey: mockKey)
+
+        XCTAssertNotNil(try? cache.value(forKey: mockKey))
+
+        XCTAssertNoThrow(try cache.saveToDisk())
+
+        let fileManager = FileManager.default
+
+        let folderURLs = fileManager.urls(
+            for: .cachesDirectory,
+            in: .userDomainMask
+        )
+
+        let fileURL = folderURLs[0].appendingPathComponent(cacheName + ".cache")
+
+        let data = try Data(contentsOf: fileURL, options: .uncached)
+
+        let cacheBox = try JSONDecoder().decode(Cache<Int, String>.self, from: data)
+
+        XCTAssertNotNil(cacheBox)
+
+        XCTAssertNoThrow(try cacheBox.value(forKey: mockKey))
     }
 }
