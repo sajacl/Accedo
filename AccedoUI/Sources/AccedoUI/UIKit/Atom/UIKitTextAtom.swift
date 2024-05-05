f9f58a69cfb4d550c0f49c0647671064327c9832
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 22:17:17 +0200

    Update UIKitTextAtom.swift

--- AccedoUI/Sources/AccedoUI/UIKit/Atom/UIKitTextAtom.swift
+++ AccedoUI/Sources/AccedoUI/UIKit/Atom/UIKitTextAtom.swift
@@ -3,13 +3,20 @@ import class UIKit.UILabel
 
 extension NS {
     public final class TextAtom: UILabel {
-        public init(_ key: LocalizedKey) {
+        public init(_ key: LocalizedKey, isUsingAutoLayout: Bool = false) {
             super.init(frame: .zero)
 
             text = key.text
-            translatesAutoresizingMaskIntoConstraints = false
+
+            translatesAutoresizingMaskIntoConstraints = !isUsingAutoLayout
         }
-        
+
+        public init(isUsingAutoLayout: Bool = false) {
+            super.init(frame: .zero)
+
+            translatesAutoresizingMaskIntoConstraints = !isUsingAutoLayout
+        }
+
         required init?(coder: NSCoder) {
             super.init(coder: coder)
         }
