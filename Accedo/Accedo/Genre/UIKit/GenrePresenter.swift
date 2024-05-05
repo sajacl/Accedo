473b985c93c2a72e726fcfa67b38b9a373bfc5f5
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 23:07:29 +0200

    Update GenrePresenter.swift

--- Accedo/Accedo/Genre/UIKit/GenrePresenter.swift
+++ Accedo/Accedo/Genre/UIKit/GenrePresenter.swift
@@ -2,12 +2,42 @@ import Foundation
 import AccedoREST
 
 final class GenrePresenter {
+    /// Light weight state machine.
+    enum State {
+        case loading
+
+        case empty
+
+        case list
+    }
+
     private unowned let view: GenreViewController
 
     private let interactor: GenreInteractor
 
+    private let internalQueue = DispatchQueue(label: "GenrePresenter.InternalQueue")
+
+    private var _state: State = .loading
+    private var state: State {
+        get {
+            internalQueue.sync {
+                _state
+            }
+        }
+
+        set {
+            internalQueue.sync {
+                _state = newValue
+            }
+        }
+    }
+
     init(view: GenreViewController, interactor: GenreInteractor) {
         self.view = view
         self.interactor = interactor
+    }
+
+    func viewDidAppear() {
+
     }
 }
