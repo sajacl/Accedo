686dad95f899c984d10ce4cbb732a625ed77ab10
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 16:58:47 +0200

    Create CacheKeyTracker.swift

---
+++ AccedoDB/Sources/AccedoDB/CacheKeyTracker.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+extension Cache {
+    final class KeyTracker: NSObject, NSCacheDelegate {
+        var keys = Set<Key>()
+
+        func cache(
+            _ cache: NSCache<AnyObject, AnyObject>,
+            willEvictObject object: Any
+        ) {
+            guard let entry = object as? Entry else {
+                return
+            }
+
+            keys.remove(entry.key)
+        }
+    }
+}
