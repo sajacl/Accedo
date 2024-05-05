0a61c993b495c2632d1bd6340c79883031c13125
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 13:02:21 +0200

    Create RESTDefaults.swift

---
+++ AccedoREST/Sources/AccedoREST/RESTDefaults.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+extension REST {
+    /// The language provided to all network requests, defaults to `en`.
+    static let language = "en"
+
+    /// API host.
+    static let apiHost = "api.themoviedb.org"
+
+    /// Default network timeout for API requests.
+    static let defaultAPINetworkTimeout: TimeInterval = 30.0
+}
