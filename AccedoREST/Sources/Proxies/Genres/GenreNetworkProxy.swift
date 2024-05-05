22cf7facecd319ffdedfe6ced73d1927acbae148
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 14:12:43 +0200

    Create GenreNetworkProxy.swift

---
+++ AccedoREST/Sources/Proxies/Genres/GenreNetworkProxy.swift
@@ -0,0 +1,76 @@
+import Foundation
+
+/// Genre list url path.
+private let genreURLPath = "/3/genre/movie/list"
+
+/// Tag that will be used for naming network operation.
+private let tag = "fetch-genre"
+
+public final class GenreNetworkProxy {
+    private let authorizationProvider: RESTAuthorizationProvider
+
+    public init(authorizationProvider: @autoclosure @escaping () throws -> REST.Authorization) {
+        self.authorizationProvider = REST.AccessTokenProvider(accessToken: authorizationProvider)
+    }
+
+    public init(authorizationProvider: RESTAuthorizationProvider) {
+        self.authorizationProvider = authorizationProvider
+    }
+
+    @available(iOS 13.0, *)
+    public func fetchGenres() async throws -> Result<GenresDecodableModel, Error> {
+        let requestHandler = REST.AnyRequestHandler { endpoint in
+            let requestFactory = REST.RequestFactory.default(
+                with: self.authorizationProvider,
+                bodyEncoder: REST.Coding.makeJSONEncoder()
+            )
+
+            return try requestFactory.createRequest(
+                for: .get,
+                pathTemplate: REST.URLPathTemplate(stringLiteral: genreURLPath)
+            )
+        }
+
+        let responseHandler = REST.defaultResponseHandler(
+            decoding: GenresDecodableModel.self,
+            with: REST.Coding.makeJSONDecoder()
+        )
+
+        let networkOperation = REST.SwiftConcurrencyNetworkDispatcher(
+            name: tag + "-async",
+            requestHandler: requestHandler,
+            responseHandler: responseHandler
+        )
+
+        return try await networkOperation.start()
+    }
+
+    public func fetchGenres(completionHandler: @escaping () -> Result<GenresDecodableModel, Error>) {
+        let requestHandler = REST.AnyRequestHandler { endpoint in
+            let requestFactory = REST.RequestFactory.default(
+                with: self.authorizationProvider,
+                bodyEncoder: REST.Coding.makeJSONEncoder()
+            )
+
+            return try requestFactory.createRequest(
+                for: .get,
+                pathTemplate: REST.URLPathTemplate(stringLiteral: genreURLPath)
+            )
+        }
+
+        let responseHandler = REST.defaultResponseHandler(
+            decoding: GenresDecodableModel.self,
+            with: REST.Coding.makeJSONDecoder()
+        )
+
+//        let networkOperation = REST.SwiftConcurrencyNetworkDispatcher(
+//            name: tag + "-closure",
+//            requestHandler: requestHandler,
+//            responseHandler: responseHandler
+//        )
+
+//        let result = try await networkOperation.start()
+//
+//        let genres = try result.get()
+    }
+}
