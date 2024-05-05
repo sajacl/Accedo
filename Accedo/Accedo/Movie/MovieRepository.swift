3af205a1299591a654614e007ab60cab6d7445f6
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 19:35:33 +0200

    Update MovieRepository.swift

--- Accedo/Accedo/Movie/MovieRepository.swift
+++ Accedo/Accedo/Movie/MovieRepository.swift
@@ -3,10 +3,17 @@ import AccedoDB
 import struct AccedoREST.MoviesAPIResponse
 import struct AccedoREST.MovieDecodableModel
 
-@DatabaseActor
+protocol MovieRepositoryInterface {
+    func getMovies() throws -> [Movie]
+
+    func upsertMovies(_ decodedMovies: [MovieDecodableModel]) throws
+}
+
 final class MovieRepository {
+    @DatabaseActor
     private let cache: Cache<Int, Movie> = Cache(name: "MovieCache")
 
+    @DatabaseActor
     func getMovies() throws -> [Movie] {
         try cache.allValues()
     }
@@ -14,6 +21,7 @@ final class MovieRepository {
     /// Adds or updates movies with the same id.
     /// - Warning: This method has small overhead (O(n) * 2, since for code clarity it uses two iteration,
     /// One for decoding and one for adding/updating in cache.
+    @DatabaseActor
     func upsertMovies(_ decodedMovies: [MovieDecodableModel]) throws {
         let mappedMovies = decodedMovies.map(Movie.init(from:))
 
@@ -21,34 +29,34 @@ final class MovieRepository {
             cache.upsert(movie, forKey: movie.id)
         }
     }
-
-    public struct Movie: Hashable {
-        public let id: Int
-        public let adult: Bool?
-        public let backdropPath: String?
-        public let genreIDS: [Int]
-        public let originalLanguage, originalTitle, overview: String?
-        public let popularity: Double?
-        public let posterPath, releaseDate, title: String?
-        public let video: Bool?
-        public let voteAverage: Double?
-        public let voteCount: Int?
-
-        fileprivate init(from decodableModel: MovieDecodableModel) {
-            id = decodableModel.id
-            adult = decodableModel.adult
-            backdropPath = decodableModel.backdropPath
-            genreIDS = decodableModel.genreIDS ?? []
-            originalLanguage = decodableModel.originalLanguage
-            originalTitle = decodableModel.originalTitle
-            overview = decodableModel.overview
-            popularity = decodableModel.popularity
-            posterPath = decodableModel.posterPath
-            releaseDate = decodableModel.releaseDate
-            title = decodableModel.title
-            video = decodableModel.video
-            voteAverage = decodableModel.voteAverage
-            voteCount = decodableModel.voteCount
-        }
-    }
 }
+
+public struct Movie: Hashable {
+    public let id: Int
+    public let adult: Bool?
+    public let backdropPath: String?
+    public let genreIDS: [Int]
+    public let originalLanguage, originalTitle, overview: String?
+    public let popularity: Double?
+    public let posterPath, releaseDate, title: String?
+    public let video: Bool?
+    public let voteAverage: Double?
+    public let voteCount: Int?
+
+    fileprivate init(from decodableModel: MovieDecodableModel) {
+        id = decodableModel.id
+        adult = decodableModel.adult
+        backdropPath = decodableModel.backdropPath
+        genreIDS = decodableModel.genreIDS ?? []
+        originalLanguage = decodableModel.originalLanguage
+        originalTitle = decodableModel.originalTitle
+        overview = decodableModel.overview
+        popularity = decodableModel.popularity
+        posterPath = decodableModel.posterPath
+        releaseDate = decodableModel.releaseDate
+        title = decodableModel.title
+        video = decodableModel.video
+        voteAverage = decodableModel.voteAverage
+        voteCount = decodableModel.voteCount
+    }
+    }
