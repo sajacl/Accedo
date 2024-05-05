3bf675fcc72276a26a5a0689388862f309672012
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 22:34:04 +0200

    Update UIKitAppDelegate.swift

--- Accedo/Accedo/main/AppDelegateComponents/UIKit/UIKitAppDelegate.swift
+++ Accedo/Accedo/main/AppDelegateComponents/UIKit/UIKitAppDelegate.swift
@@ -3,4 +3,22 @@ import UIKit
 
 final class UIKitAppDelegate: AppDelegateType {
     var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        let _window = UIWindow()
+
+        _window.rootViewController = createMainViewController()
+        _window.makeKeyAndVisible()
+
+        window = _window
+
+        return true
+    }
+
+    private func createMainViewController() -> UIViewController {
+        GenreViewController()
+    }
 }
