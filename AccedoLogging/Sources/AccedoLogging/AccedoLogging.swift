168d7d82107beab794ff995c6acbc342ab8948f4
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 13:01:09 +0200

    Introduce `Logging` protocol.

--- AccedoLogging/Sources/AccedoLogging/AccedoLogging.swift
+++ AccedoLogging/Sources/AccedoLogging/AccedoLogging.swift
@@ -1,10 +1,29 @@
-import OSLog
+/// It will be exported automatically on depending modules.
+/// (No need to import `OSLog` on consumer modules)
+@_exported import OSLog
+import Foundation
 
 /*
  I've decided to move super simple with logging strategy.
  I will keep it as simple as possible,
  It will be a simple extension on OSLog logger for achieving a decoupled log system.
  */
+
+/// <#Description#>
+private let networkCategory = "Network"
+
+/// <#Description#>
+private let viewCategory = "View"
+
+public protocol Logging {
+    func debug(metadata: String?, message: @autoclosure () -> String)
+
+    func notice(metadata: String?, message: @autoclosure () -> String)
+
+    func error(metadata: String?, message: @autoclosure () -> String)
+
+    func error(metadata: String?, error: some Error, message: @autoclosure () -> String)
+}
 
 @available(iOS 14.0, *)
 extension OSLog {
