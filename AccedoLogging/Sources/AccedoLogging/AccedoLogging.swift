b2d832737bc6cac51da70cc11a17d4ca71795f33
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 12:15:24 +0200

    Add logging system/components.

--- AccedoLogging/Sources/AccedoLogging/AccedoLogging.swift
+++ AccedoLogging/Sources/AccedoLogging/AccedoLogging.swift
@@ -1,2 +1,19 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import OSLog
+
+/*
+ I've decided to move super simple with logging strategy.
+ I will keep it as simple as possible,
+ It will be a simple extension on OSLog logger for achieving a decoupled log system.
+ */
+
+@available(iOS 14.0, *)
+extension OSLog {
+    /// Name of subsystem/Subsystem tag.
+    private static let subSystem = "com.sajacl.Accedo"
+
+    /// Logger component for logging network interactions.
+    public static let networkLogger = Logger(subsystem: subSystem, category: "Network")
+
+    /// Logger component for logging view interactions.
+    public static let viewLogger = Logger(subsystem: subSystem, category: "View")
+}
