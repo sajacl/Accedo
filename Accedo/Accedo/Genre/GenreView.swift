a000aa8ec837b31aab48e8dfb59a9d076a24f5d5
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 19:20:26 +0200

    Rework `GenreView`

--- Accedo/Accedo/Genre/GenreView.swift
+++ Accedo/Accedo/Genre/GenreView.swift
@@ -3,12 +3,54 @@ import SwiftUI
 import AccedoUI
 
 struct GenreView: View {
-    @StateObject private var viewModel = GenreViewModel(authorization: "6f83e26e82a37507bf29b27ff511f452")
+    @StateObject private var viewModel: GenreViewModel
+
+    private let userInterfaceIdiom: UIUserInterfaceIdiom
+
+    public init(
+        viewModel: GenreViewModel,
+        userInterfaceIdiom: UIUserInterfaceIdiom = UIDevice.current.userInterfaceIdiom
+    ) {
+        self._viewModel = StateObject(wrappedValue: viewModel)
+        self.userInterfaceIdiom = userInterfaceIdiom
+    }
 
     var body: some View {
-        List(viewModel.genres) { genre in
-            SU.GenreCell(title: genre.name)
+        listView
+            .onAppear(perform: viewModel.viewDidAppear)
+    }
+
+    @ViewBuilder
+    private var listView: some View {
+        if userInterfaceIdiom == .phone {
+            listCell
+        } else {
+            gridCell
         }
-        .onAppear(perform: viewModel.viewDidAppear)
+    }
+
+    private var listCell: some View {
+        List(viewModel.genres) { genre in
+            LazyVStack {
+                SU.GenreCell(title: genre.name)
+            }
+        }
+    }
+
+    private var gridCell: some View {
+        ScrollView{
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))], spacing: 20) {
+                ForEach(viewModel.genres, id: \.self) { genre in
+                    Button(
+                        action: {
+
+                        },
+                        label: {
+                            SU.GenreCell(title: genre.name)
+                        }
+                    )
+                }
+            }
+        }
     }
 }
