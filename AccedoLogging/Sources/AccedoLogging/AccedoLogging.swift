fbc6a06317b732d921c2cc216055f6cae83c6aa1
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 13:01:47 +0200

    Add `CustomLogger`, conform `OSLog.Logger` to `Logging`.

--- AccedoLogging/Sources/AccedoLogging/AccedoLogging.swift
+++ AccedoLogging/Sources/AccedoLogging/AccedoLogging.swift
@@ -31,8 +31,75 @@ extension OSLog {
     private static let subSystem = "com.sajacl.Accedo"
 
     /// Logger component for logging network interactions.
-    public static let networkLogger = Logger(subsystem: subSystem, category: "Network")
+    public static let networkLogger = Logger(subsystem: subSystem, category: networkCategory)
 
     /// Logger component for logging view interactions.
-    public static let viewLogger = Logger(subsystem: subSystem, category: "View")
+    public static let viewLogger = Logger(subsystem: subSystem, category: viewCategory)
+}
+
+@available(iOS 14.0, *)
+extension Logger: Logging {
+    public func debug(metadata: String? = nil, message: @autoclosure () -> String) {
+        let _message = message()
+        debug("\(_message)")
+    }
+    
+    public func notice(metadata: String? = nil, message: @autoclosure () -> String) {
+        let _message = message()
+        notice("\(_message)")
+    }
+    
+    public func error(metadata: String? = nil, message: @autoclosure () -> String) {
+        let _message = message()
+        log(level: .error, "\(_message)")
+    }
+
+    public func error(
+        metadata: String?,
+        error: some Error,
+        message: @autoclosure () -> String
+    ) {
+        let error = message()
+        log(level: .error, "\(error)")
+    }
+}
+
+public struct CustomLogger: Logging {
+    private let logResolver: (String) -> Void
+
+    private let category: String
+
+    public static let networkLogger = CustomLogger(category: networkCategory)
+    
+    public static let viewLogger = CustomLogger(category: viewCategory)
+
+    public init(category: String, logger: @escaping (String) -> Void = { print($0) }) {
+        self.category = category
+        logResolver = logger
+    }
+
+    public func debug(metadata: String? = nil, message: @autoclosure () -> String) {
+        dump("Debug", message())
+    }
+
+    public func notice(metadata: String? = nil, message: @autoclosure () -> String) {
+        dump("Info", message())
+    }
+
+    public func error(metadata: String? = nil, message: @autoclosure () -> String) {
+        dump("Error", message())
+    }
+
+    public func error(
+        metadata: String? = nil,
+        error: some Error,
+        message: @autoclosure () -> String
+    ) {
+        let _message = "[Underlying error description: \(error.localizedDescription)] [Error message: \(message())]"
+        dump("Error", _message)
+    }
+
+    private func dump(_ arg: String,_ message: String) {
+        logResolver("-[\(arg)] [\(category)] \(message)")
+    }
 }
