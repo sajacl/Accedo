5b59dc50c9d31fa1558b421ee9db34845f99960c
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 15:12:00 +0200

    Create MoviesDecodableModel.swift

---
+++ AccedoREST/Sources/Proxies/Movies/MoviesDecodableModel.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+// MARK: - Movies
+public struct MoviesAPIResponse: Codable {
+    public let page: Int
+    public let movies: [MovieDecodableModel]
+    public let totalPages, totalResults: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case page
+        case movies
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+// MARK: - Movie
+public struct MovieDecodableModel: Codable {
+    public let id: Int
+    public let adult: Bool?
+    public let backdropPath: String?
+    public let genreIDS: [Int]?
+    public let originalLanguage, originalTitle, overview: String?
+    public let popularity: Double?
+    public let posterPath, releaseDate, title: String?
+    public let video: Bool?
+    public let voteAverage: Double?
+    public let voteCount: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIDS = "genre_ids"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
