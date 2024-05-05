0c4f4dfab2d4d88b5622cebf62107ac736a134cb
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-05-05 22:34:01 +0200

    Update SwiftUIAppDelegate.swift

--- Accedo/Accedo/main/AppDelegateComponents/SwiftUI/SwiftUIAppDelegate.swift
+++ Accedo/Accedo/main/AppDelegateComponents/SwiftUI/SwiftUIAppDelegate.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+/// Dummy class purely for separation of concerns, that shows this can be the ground zero for `SwiftUI` configurations,
+/// that are depending on old logics.
 final class SwiftUIAppDelegate: AppDelegateType {
-    
+    // no-op
 }
